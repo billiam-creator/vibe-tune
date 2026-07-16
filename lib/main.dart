a8aecb4e0f5cd75b6d72e7efc51665bbdfbbeedd
unknown <bushg5200@gmail.com>
2026-07-16 23:01:50 +0300

    intergrate api and connected with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'theme/app_theme.dart';
 import 'screens/app_shell.dart';
+import 'services/app_state.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
@@ -11,12 +12,12 @@ void main() {
     DeviceOrientation.landscapeLeft,
     DeviceOrientation.landscapeRight,
   ]);
+  await AppState.instance.init();
   runApp(const VibeTuneApp());
 }
 
 class VibeTuneApp extends StatelessWidget {
   const VibeTuneApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
